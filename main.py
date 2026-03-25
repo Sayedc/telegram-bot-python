af844ceba9a75d97b82a01c46ccc56effc74f763
Sayed <102337556+Sayedc@users.noreply.github.com>
2026-03-25 12:19:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,29 @@ import os
 
 TOKEN = os.getenv("BOT_TOKEN")
 
+if not TOKEN:
+    print("❌ BOT_TOKEN مش موجود")
+    exit()
+
 bot = telebot.TeleBot(TOKEN)
+
+print("🚀 Bot started...")
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "👋 ازيك يزمكس! ابعت لينك وأنا هحملهولك 🔥")
+    bot.reply_to(message, "👋 ازيك يا نجم! ابعت لينك وانا احملهولك 🔥")
 
 @bot.message_handler(commands=['hello'])
 def hello(message):
-    bot.reply_to(message, "❤️ عامل ايه يا نجم؟")
+    bot.reply_to(message, "❤️ حبيبي عامل ايه؟")
 
 @bot.message_handler(func=lambda message: True)
 def all_messages(message):
-    bot.reply_to(message, "📩 وصلتني الرسالة يا معلم")
+    bot.reply_to(message, "📩 وصلت رسالتك يا معلم")
 
-print("Bot is running...")
-
-bot.infinity_polling()
+# 👇 أهم سطر (مع حماية من الكراش)
+while True:
+    try:
+        bot.infinity_polling(timeout=10, long_polling_timeout=5)
+    except Exception as e:
+        print(f"❌ Error: {e}")
