698a5661fec843c1d1da8a7c36777488b324330f
Sayed <102337556+Sayedc@users.noreply.github.com>
2026-06-30 02:53:12 +0300

    Refactor user repository to use JSON storage
    Refactor user management to use JSON file instead of database. Added functions to initialize, save, and retrieve user data.

--- database/user_repository.py
+++ database/user_repository.py
@@ -1,36 +1,49 @@
+import json
+import os
 from datetime import datetime
-from database.db import get_connection
 
-def add_user(user_id, username):
-    conn = get_connection()
-    cur = conn.cursor()
+DB_FILE = "bot_database.json"
 
-    cur.execute("""
-    INSERT OR IGNORE INTO users
-    (user_id, username, first_seen, last_seen)
-    VALUES (?, ?, ?, ?)
-    """, (
-        user_id,
-        username,
-        datetime.now().isoformat(),
-        datetime.now().isoformat()
-    ))
 
-    conn.commit()
-    conn.close()
+def init_db():
+    if not os.path.exists(DB_FILE):
+        with open(DB_FILE, "w") as f:
+            json.dump({
+                "users": {},
+                "total": 0,
+                "daily": 0,
+                "last_date": str(datetime.now().date())
+            }, f, indent=2)
 
-def update_last_seen(user_id):
-    conn = get_connection()
-    cur = conn.cursor()
 
-    cur.execute("""
-    UPDATE users
-    SET last_seen = ?
-    WHERE user_id = ?
-    """, (
-        datetime.now().isoformat(),
-        user_id
-    ))
+async def save_user(user_id, username):
+    with open(DB_FILE, "r+") as f:
+        data = json.load(f)
 
-    conn.commit()
-    conn.close()
+        if str(user_id) not in data["users"]:
+            data["users"][str(user_id)] = {
+                "name": username,
+                "first_seen": str(datetime.now()),
+                "last_seen": str(datetime.now()),
+                "downloads": 0,
+                "fav_platform": "None",
+                "platforms": {},
+                "blocked": False
+            }
+
+        else:
+            data["users"][str(user_id)]["last_seen"] = str(datetime.now())
+
+        f.seek(0)
+        json.dump(data, f, indent=2)
+        f.truncate()
+
+
+def get_data():
+    with open(DB_FILE, "r") as f:
+        return json.load(f)
+
+
+def save_data(data):
+    with open(DB_FILE, "w") as f:
+        json.dump(data, f, indent=2)
