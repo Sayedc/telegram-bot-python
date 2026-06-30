d8c96326ff7bfade1f53affb5b6edd68cfdcac20
Sayed <102337556+Sayedc@users.noreply.github.com>
2026-06-30 12:39:04 +0300

    Initialize utils package with imports

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,21 @@
-
+# utils/__init__.py
+from .helpers import (
+    extract_link,
+    get_platform,
+    get_video_info,
+    get_random_processing_text,
+    get_random_success_text,
+    get_random_error_text,
+    get_random_sticker,
+)
+from .constants import (
+    SIGNATURE,
+    VERSION,
+    START_TIME,
+    WELCOME_RESPONSES,
+    PROCESSING_TEXTS,
+    SUCCESS_TEXTS,
+    ERROR_TEXTS,
+    STICKERS,
+)
+from .messages import get_response
